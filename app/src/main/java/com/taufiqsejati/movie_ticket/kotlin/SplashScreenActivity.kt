a061e03184189d8d44576547a954eaea4981f29f
taufiqsejati <taufiq_sejati@rocketmail.com>
2022-12-10 19:52:10 +0700

    wip sign in page

--- app/src/main/java/com/taufiqsejati/movie_ticket/kotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/taufiqsejati/movie_ticket/kotlin/SplashScreenActivity.kt
@@ -3,9 +3,9 @@ package com.taufiqsejati.movie_ticket.kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
--- app/src/main/java/com/taufiqsejati/movie_ticket/kotlin/onboarding/OnboardingTreeActivity.kt
+++ app/src/main/java/com/taufiqsejati/movie_ticket/kotlin/onboarding/OnboardingTreeActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.taufiqsejati.movie_ticket.kotlin.R
 
-class OnboardingOneActivity : AppCompatActivity() {
+class OnboardingTreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding_one)
+        setContentView(R.layout.activity_onboarding_tree)
     }
 }
